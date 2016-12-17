5c4856e52eef90c3af2ed49a554c0a5fc8f3a6c7
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-12-17 08:51:54 -0600

    set the input properties of task class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import  { Task } from './model/task'
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    @Input() task: Task;
+ }
